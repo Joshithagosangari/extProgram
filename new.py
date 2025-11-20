e271028db60fc24dcf4264243bd632dd03cf775e
joshithagosangari <gosangarijoshitha@gmail.com>
2025-11-20 11:36:08 +0530

    Initial commit

---
+++ new.py
@@ -0,0 +1,9 @@
+a=40
+b=20
+
+print("**Arithmetic operations**")
+print("Sum of",a,"&",b,":",a+b)
+print("Difference of",a,"&",b,":",a-b)
+print("product of",a,"&",b,":",a*b)
+print("Division of",a,"&",b,":",a/b)
+print("modulo operation of",a,"&",b,":",a%b)
